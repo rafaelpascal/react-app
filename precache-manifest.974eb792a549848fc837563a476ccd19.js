e8e211a942255c1468c0b5580c4b4ed129181dd3
rafaelpascal <67458341+rafaelpascal@users.noreply.github.com>
2021-02-05 10:34:28 +0100

    Updates

---
+++ precache-manifest.974eb792a549848fc837563a476ccd19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d6f5430fd31bb82f37e8d0a0a22f36",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8f0bfae7c22eb8336f83",
+    "url": "/react-app/static/css/main.dbb0d8dc.chunk.css"
+  },
+  {
+    "revision": "4aaa7f51e1fac2ce9598",
+    "url": "/react-app/static/js/2.08019749.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/react-app/static/js/2.08019749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0bfae7c22eb8336f83",
+    "url": "/react-app/static/js/main.7a2e04bf.chunk.js"
+  },
+  {
+    "revision": "fd331c96275bca7ca800",
+    "url": "/react-app/static/js/runtime-main.0c087f40.js"
+  }
+]);
